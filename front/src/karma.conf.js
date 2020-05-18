088fcf68c12fc21178b5abd5fad71b2b4bf32b62
Nacho Martínez <44577104+NachoMC@users.noreply.github.com>
2020-05-18 14:05:41 +0200

    chrome

--- front/src/karma.conf.js
+++ front/src/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+    browsers: ['ChromeHeadless'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
