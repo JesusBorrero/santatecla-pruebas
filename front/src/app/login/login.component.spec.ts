ff19e317f14dec64da20a49edd93964e0003f752
Nacho Martínez <44577104+NachoMC@users.noreply.github.com>
2020-05-18 13:51:24 +0200

    chrome

--- front/src/app/login/login.component.spec.ts
+++ front/src/app/login/login.component.spec.ts
@@ -1,13 +1,15 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing';
 import {FormsModule} from '@angular/forms';
-import {MatDialogModule, MatFormFieldModule, MatIconModule, MatTooltipModule} from '@angular/material';
+import {MatDialogModule, MatFormFieldModule, MatIconModule, MatInputModule, MatTooltipModule} from '@angular/material';
 import {HttpClientModule} from '@angular/common/http';
 import {LoginService} from '../auth/login.service';
 import {LoginComponent} from './login.component';
 import {MenuComponent} from '../menu/menu.component';
 import {AppComponent} from '../app.component';
 import {By} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {Observable} from 'rxjs';
 
 describe('Login component', () => {
 
@@ -15,15 +17,36 @@ describe('Login component', () => {
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async(() => {
+    class MockLoginService extends LoginService {
+      error = false;
+
+      login(user: string, pass: string) {
+        if (user === 'profesor' && pass === 'password') {
+          this.error = true;
+        }
+
+        return Observable.create(observer => {
+          if (this.error) {
+            observer.error(new Error());
+          } else {
+            observer.next();
+          }
+          observer.complete();
+        });
+      }
+    }
+
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
         FormsModule,
         MatIconModule,
         MatFormFieldModule,
+        MatInputModule,
         MatTooltipModule,
         MatDialogModule,
-        HttpClientModule
+        HttpClientModule,
+        BrowserAnimationsModule
       ],
       providers: [LoginService],
       declarations: [
@@ -44,17 +67,31 @@ describe('Login component', () => {
   });
 
   it('should login successfully', () => {
-    let teacherInput = fixture.debugElement.query(By.css('#username')).nativeElement;
-    teacherInput.value = 'profesor';
+    spyOn(component, 'login');
 
-    let passwordInput = fixture.debugElement.query(By.css('#password')).nativeElement;
-    passwordInput.value = 'profesor';
+    fixture.debugElement.query(By.css('#username')).nativeElement.value = 'profesor';
+    fixture.debugElement.query(By.css('#password')).nativeElement.value = 'profesor';
+    fixture.debugElement.query(By.css('#send-login-button')).triggerEventHandler('click', null);
 
-    let submitButton = fixture.debugElement.query(By.css('#login-button')).nativeElement;
-    submitButton.click();
+    fixture.detectChanges();
 
     fixture.whenStable().then(() => {
       expect(component.login).toHaveBeenCalled();
+    });
+  });
+
+  it('should not login', () => {
+    spyOn(window, 'alert');
+
+    fixture.debugElement.query(By.css('#username')).nativeElement.value = 'profesor';
+    fixture.debugElement.query(By.css('#password')).nativeElement.value = 'password';
+    fixture.debugElement.query(By.css('#send-login-button')).triggerEventHandler('click', null);
+
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      expect(component.login).toHaveBeenCalled();
+      expect(window.alert).toHaveBeenCalledWith('Invalid user or password');
     });
   });
 
